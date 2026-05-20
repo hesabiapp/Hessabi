76ce47f96b81c5d82d9c694d6629c23f2c926401
hesabiapp <tryhesabi.app@gmail.com>
2026-05-20 22:08:54 +0300

    fix: Server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,15 +1,15 @@
-import app from "./express.js";
 import { config } from "dotenv";
-import { mongoClient } from "./config/mongo.js";
-
 config();
 
-const port = process.env.PORT || 3000
+import app from "./express.js";
+import { mongoClient } from "./config/mongo.js";
+
+const port = process.env.PORT || 3000;
 
 mongoClient().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`)
-    })
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 }).catch((err) => {
-    console.log(err)
-})
+  console.log(err);
+});
